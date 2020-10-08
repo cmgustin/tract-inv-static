145a72570daffe89e35fab87277f5a7c03881e2a
Chris Gustin <gustin.chris@gmail.com>
2020-10-08 17:55:36 -0400

    Finish main menu active styles.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
     $('.sub-menu .nav-item').removeClass('active');
     $(this).parent('.nav-item').addClass('active');
   })
+
+  $('.main-menu__link').on('click', function() {
+    $('.main-menu__item').removeClass('active');
+    $(this).parent('.main-menu__item').addClass('active');
+  })
 });
